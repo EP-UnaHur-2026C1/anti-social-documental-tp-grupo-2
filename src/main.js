3bb562ea9a230236e4e5e6cabfa838f088d3f97b
Tomas Lena <tomaslena10@gmail.com>
2026-06-24 14:00:06 -0300

    documentacion Swagger agregada e implementada

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ const express = require('express');
 const connectDB = require('../config/db');
 const path = require('path');
 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
 const userRoutes = require('./routes/user.routes');
 const postRoutes = require('./routes/post.routes');
 const commentRoutes = require('./routes/comment.routes');
@@ -13,6 +16,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 
+const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
 
 app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
@@ -21,9 +25,10 @@ app.use('/tags', tagRoutes);
 app.use('/assets', express.static(path.join(__dirname, '../assets')));
 
 //  -- Esto es para poder acceder a la documentación desde una ruta -- 
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
   connectDB();
   console.log(`Servidor escuchando en el puerto ${PORT}`);
+  console.log(`Documentación disponible en http://localhost:${PORT}/api-docs`);
 });
