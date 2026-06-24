73ba341eccab7d1692b560491c3f49fe43d2be0c
Jorditan <matiasowjordan@gmail.com>
2026-06-23 22:55:17 -0300

    correccion tags y empezando collections en postman

--- src/middlewares/valideteResource.js
+++ src/middlewares/valideteResource.js
@@ -62,7 +62,7 @@ const validateImageExists = async (req, res, next) => {
 
 const validateTagExists = async (req, res, next) => {
   try {
-    const tag = await Tag.findById(req.params.tagId || req.body.tagId);
+    const tag = await Tag.findById(req.params.id);
     if (!tag) return res.status(404).json({ error: 'Tag no encontrado' });
     req.tag = tag;
     next();
@@ -104,7 +104,6 @@ module.exports = {
   validateUserExists,
   validateFollowedUserExists,
   validateImageExists,
-  validateImageUrl,
   validateTagExists,
   validateTagNotAssociated,
   validateTagAssociated,
